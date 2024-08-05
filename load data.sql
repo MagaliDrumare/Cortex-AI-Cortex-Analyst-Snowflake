3bffbfdcef49d4a775bd45af8d569f390e7dbc7e
MD <mag250475@gmail.com>
2024-08-05 18:35:19 +0200

    Create load data.sql

---
+++ load data.sql
@@ -0,0 +1,20 @@
+CREATE or REPLACE file format csvformat
+  SKIP_HEADER = 1
+  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+  type = 'CSV';
+
+CREATE or REPLACE stage support_tickets_data_stage
+  file_format = csvformat
+  url = 's3://sfquickstarts/finetuning_llm_using_snowflake_cortex_ai/';
+
+CREATE or REPLACE TABLE SUPPORT_TICKETS (
+  ticket_id VARCHAR(60),
+  customer_name VARCHAR(60),
+  customer_email VARCHAR(60),
+  service_type VARCHAR(60),
+  request VARCHAR,
+  contact_preference VARCHAR(60)
+);
+
+COPY into SUPPORT_TICKETS
+  from @support_tickets_data_stage;
